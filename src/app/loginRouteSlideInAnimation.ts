539f2a78be11348215edb102a1f7f53efe229dc3
Michael Burger <michael.burger@orma-solutions.it>
2019-03-11 12:05:55 +0100

    update dependencies

--- src/app/loginRouteSlideInAnimation.ts
+++ src/app/loginRouteSlideInAnimation.ts
@@ -21,8 +21,7 @@ export const loginRouteSlideInAnimation =
           { optional: true }
         )
       ])
-    ])
-    ,
+    ]),
     transition(':decrement', [
       query(
         ':enter',
